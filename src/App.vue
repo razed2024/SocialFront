9162181b9095fac7bb643b3c7ad08d433747c0d4
razed2024 <razed3322@gamil.com>
2024-08-03 08:44:55 +0600

    first commit

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<script setup>
+import { RouterLink, RouterView,useRoute } from 'vue-router'
+const route = useRoute();
+</script>
+
+<template>
+  <div>
+
+    <header class="bg-white shadow">
+      <div class="wrapper">
+    
+      <nav>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+
+      </nav>
+    </div>
+  </header>
+  
+  </div>
+  
+  <RouterView />
+</template>
+
+<style>
+
+</style>
